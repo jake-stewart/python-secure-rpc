5b2be40a9250b7a5d58f41dc32111ec1ff83af96
jake-stewart <83528263+jake-stewart@users.noreply.github.com>
2021-05-15 03:03:45 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 from rpc_client import connect
 import tkinter as tk
-from tkinter import ttk
+from widgets import ScrollableFrame, WrappingLabel
 
 HOST = 'localhost'
 PORT = 8080
@@ -9,33 +9,6 @@ POLLING_DELAY = 250
 TITLE_TEXT = "Honours Study Evaluator"
 SUBTITLE_TEXT = "Enter your unit codes and their marks, " + \
     "then click confirm to see if you are elegable for honours study."
-
-class ScrollableFrame(ttk.Frame):
-    def __init__(self, container, *args, **kwargs):
-        super().__init__(container, *args, **kwargs)
-        canvas = tk.Canvas(self, highlightthickness=0)
-        scrollbar = ttk.Scrollbar(self, orient="vertical", command=canvas.yview)
-        self.scrollable_frame = ttk.Frame(canvas)
-
-        self.scrollable_frame.bind(
-            "<Configure>",
-            lambda e: canvas.configure(
-                scrollregion=canvas.bbox("all")
-            )
-        )
-
-        canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-
-        canvas.configure(yscrollcommand=scrollbar.set)
-
-        canvas.pack(side="left", fill="both", expand=True)
-        scrollbar.pack(side="right", fill="y")
-
-class WrappingLabel(tk.Label):
-    '''a type of Label that automatically adjusts the wrap to the size'''
-    def __init__(self, master=None, **kwargs):
-        tk.Label.__init__(self, master, **kwargs)
-        self.bind('<Configure>', lambda e: self.config(wraplength=self.winfo_width()))
 
 
 class UnitsField(ScrollableFrame):
